9fc1a923bbe723b0cd6ea3419e967f730955d0ce
pen00000tw <pen00000tw@users.noreply.github.com>
2019-11-21 03:51:49 +0800

    20191121 update

--- SVM.py
+++ SVM.py
@@ -61,18 +61,42 @@ SVM.train(train1, cv2.ml.ROW_SAMPLE, responses1)
 
 
 #測試(90筆)
+f = open('classification.csv','w')
+f.write('測試資料,預測答案,對錯,預測正確數\n')
 count = 0
 result = SVM.predict(test1)
 for i in range(30):
     if result[1][i] == [1.]:
         count+=1
+        f.write('band neutrophile,band neutrophile,對,%d\n'%count)
+    elif result[1][i] == [2.]:
+        f.write('band neutrophile,eosinophile,錯,%d\n'%count)
+    else:
+        f.write('band neutrophile,lymphocyte,錯,%d\n'%count)
+
+
 result = SVM.predict(test2)
 for i in range(30):
-    if result[1][i] == [2.]:
+    if result[1][i] == [1.]:
+        f.write('eosinophile,band neutrophile,錯,%d\n'%count)
+    elif result[1][i] == [2.]:
         count+=1
+        f.write('eosinophile,eosinophile,對,%d\n'%count)
+    else:
+        f.write('eosinophile,lymphocyte,錯,%d\n'%count)
+
+
 result = SVM.predict(test3)
 for i in range(30):
-    if result[1][i] == [3.]:
+    if result[1][i] == [1.]:
+        f.write('lymphocyte,band neutrophile,錯,%d\n'%count)
+    elif result[1][i] == [2.]:
+        f.write('lymphocyte,eosinophile,錯,%d\n'%count)
+    else:
         count+=1
-
+        f.write('lymphocyte,lymphocyte,對,%d\n'%count)
+f.write('\n\n')
+f.write('分類成功率:,%.2f'%((count / 90) * 100))
+f.write(',預測成功總數:,%d'%count)
+f.write(',測試資料總數:,90')
 print('分類成功率:%.2f'%((count / 90) * 100))
